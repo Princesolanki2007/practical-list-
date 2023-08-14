3d49134352fa14c99c775050ea37c2ce9d8943d6
Princesolanki2007 <141326691+Princesolanki2007@users.noreply.github.com>
2023-08-14 15:45:01 +0530

    Create Program_5.cpp

---
+++ list_2/Program_5.cpp
@@ -0,0 +1,14 @@
+  #include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	// function setw & endl
+	float m = 10.5555;
+	cout<<"I am Prince Solanki "<<endl;
+	cout<<"En_no    220130318032 "<<endl;
+	cout<<"---------------------"<<endl;
+	//cout<<"The Value of A is ::"<<m;
+ 	cout<<"This is for function setprecision : "<<setprecision(8)<<m; // function setw 
+	return 0;
+}
