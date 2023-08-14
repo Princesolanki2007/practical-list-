95cc5c1c21abb6073159354f8c13d70e15b9d648
Princesolanki2007 <141326691+Princesolanki2007@users.noreply.github.com>
2023-08-14 15:38:54 +0530

    Create Program_2.cpp

---
+++ list_2/Program_2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int m = 10;// Global variable 
+main()
+{
+	// Scope resolution program 
+	cout<<"I am Prince Soalnki"<<endl;
+	cout<<"En_no    220130318032 "<<endl;
+	cout<<"---------------------"<<endl;
+	cout<<"value of Global variable is m & its value  ::  "<<::m;// scop resolution 
+	return 0;
+}
