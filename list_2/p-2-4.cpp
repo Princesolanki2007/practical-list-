106286481842a132b8cd5b42ef6ad311ed176df6
Princesolanki2007 <141326691+Princesolanki2007@users.noreply.github.com>
2023-08-27 10:12:12 +0530

    Add files via upload

---
+++ list_2/p-2-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 10;
+    cout<<"I am Prince Solanki "<<endl;
+	cout<<"En_no    220130318032 "<<endl;
+	cout<<"The value of a :: "<<setw(5)<<a<<endl;
+    cout<<"The value of b :: "<<setw(5)<<b<<endl;
+    
+    return 0;
+
+}
+
