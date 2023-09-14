50f90e6fccb09f4dc179ac4c401c69f0478e2659
Princesolanki2007 <141326691+Princesolanki2007@users.noreply.github.com>
2023-09-14 08:53:26 +0530

    Add files via upload

---
+++ list_3/p-3-2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+struct student
+{
+	char a;
+	float c;
+	char b;
+};
+int main()
+{
+	cout<<"Prince Solanki"<<endl;
+	cout<<"220130318032"<<endl;
+	cout<<"-------------"<<endl;
+	cout<<"sizeof(student) is ::"<<sizeof(student);
+	return 0;
+}
