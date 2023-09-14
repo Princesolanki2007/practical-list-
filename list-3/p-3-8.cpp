78407dc61166773bc70145a1cbbe6d0bfc4c7b17
Princesolanki2007 <141326691+Princesolanki2007@users.noreply.github.com>
2023-09-14 08:45:23 +0530

    Add files via upload

---
+++ list-3/p-3-8.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class account
+{
+	public:
+		float acc_no;
+		float balance;
+		string branch_code;
+		void get_data();
+		void put_data();
+};
+int main()
+{
+	account A;
+	
+	cout<<"Enter The Account Number ::";
+	cin>>A.acc_no;
+	cout<<"Enter The Branch Code ::";
+	cin>>A.branch_code;
+	cout<<"Enter The Balance ::";
+	cin>>A.balance;
+	
+	cout<<"The Account Number is ::"<<A.acc_no<<endl;
+	cout<<"The Brach Code is ::"<<A.branch_code<<endl;
+	cout<<"The Balance Of This Account is ::"<<A.balance;
+	
+	return 0;
+}
