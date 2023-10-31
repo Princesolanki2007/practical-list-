3b66c05e8d947016153b5e3aec399d1efcaff65c
Princesolanki2007 <141326691+Princesolanki2007@users.noreply.github.com>
2023-10-31 13:53:34 +0530

    Add files via upload

---
+++ PRACTICAL LIST_4/p-4-9.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class add
+{
+	int a,b;
+	friend  void getdata(add &n);
+	
+	public :
+		
+	void putdata()
+	{
+		cout<<"Sum of A + B is ::"<<a+b;
+	}
+};
+void getdata(add &n)
+{
+	cout<<"Enter A ::";
+	cin>>n.a;
+	cout<<"Enter B ::";
+	cin>>n.b;
+}
+
+int main()
+{
+		cout<<"Prince Solnki"<<endl;
+	cout<<"220130318032"<<endl;
+	add n;
+	getdata(n);
+	n.putdata();
+	
+	return 0;
+}
+
