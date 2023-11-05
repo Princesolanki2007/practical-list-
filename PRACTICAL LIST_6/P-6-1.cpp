329d719d90c68acfdd9ae17ac8b8de683d279caa
Princesolanki2007 <141326691+Princesolanki2007@users.noreply.github.com>
2023-11-05 12:23:00 +0530

    Add files via upload

---
+++ PRACTICAL LIST_6/P-6-1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ofstream filestream("test.txt");
+	if(filestream.is_open())
+	{
+		filestream<<"Welcome to the Dr. Subhash University";
+		filestream<<"This is C++ Subject.";
+		filestream.close();
+	}
+	else
+	{
+		cout<<"File Opening is fail";
+	}
+	return 0;
+}
